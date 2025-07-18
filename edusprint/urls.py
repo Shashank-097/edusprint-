85e64ecd615d0f5f5a6db3cdd2dfc4c5114740c1
Shashank-097 <tshashank037@gmail.com>
2025-07-18 08:13:52 +0530

    Update urls.py

--- edusprint/urls.py
+++ edusprint/urls.py
@@ -20,10 +20,15 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-     path('api/portfolio/', include('portfolio.urls')),
+    path('api/portfolio/', include('portfolio.urls')),
+    path('api/services/', include('service.urls')),  
 ]
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
