02a67d5d28f85fa7b5e99a010c2ecb119dfc19bc
Rishabhnagar1 <rishabhnagar343@gmail.com>
2025-07-18 21:35:26 +0530

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'contactdb',
+        'USER': 'root',
+        'PASSWORD': '',  
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
